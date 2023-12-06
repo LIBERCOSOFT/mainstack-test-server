c1ce3f85c22747ba64c770460f4e418711626e8a
LIBERCOSOFT <akinspreciousola@gmail.com>
2023-12-06 15:26:32 +0100

    Finish all CRUD controllers

--- src/controllers/productControllers.ts
+++ src/controllers/productControllers.ts
@@ -12,13 +12,94 @@ const createProduct = asyncHandler(async (req: Request, res: Response) => {
     });
     await newProduct.save();
     res.status(201).json({
-      message: "Product Added Successfully",
+      message: 'Product Added Successfully',
       data: newProduct
     });
   } catch (error: any) {
     console.error(error);
-    res.status(500).send('Internal Server Error');
+    res.status(500).json({
+      message: 'Internal Server Error'
+    });
   }
 });
 
-export { createProduct };
+const getOneProduct = asyncHandler(async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  try {
+    const product = await Product.findById(id);
+    if (!product) {
+      res.status(404).json({
+        message: 'Product not found'
+      });
+      return;
+    }
+    res.status(200).json({
+      message: 'Product Found',
+      data: product
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: 'Internal Server Error'
+    });
+  }
+});
+
+const getAllProducts = asyncHandler(async (req: Request, res: Response) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json({
+      message: 'All products retrieved',
+      data: products
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: 'Internal Server Error'
+    });
+  }
+});
+
+const editProduct = asyncHandler(async (req: Request, res: Response) => {
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!updatedProduct) {
+      res.status(404).json({
+        message: 'Product not found'
+      });
+      return;
+    }
+    res.status(200).json({
+      message: 'Product updated successfully',
+      data: updatedProduct,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: 'Internal Server Error'
+    });
+  }
+});
+
+const deleteProduct = asyncHandler(async (req: Request, res: Response) => {
+  try {
+    const product = await Product.findByIdAndDelete(req.params.id);
+    if (!product) {
+      res.status(404).json({
+        message: 'Product not found'
+      });
+      return;
+    }
+    res.status(204).json({
+      message: 'Product successfully deleted'
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: 'Internal Server Error'
+    });
+  }
+});
+
+export { createProduct, getOneProduct, getAllProducts, editProduct, deleteProduct };
